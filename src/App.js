2fd6799091c5e0266614946b976154f832e2c982
GlindaRose <45208607+GlindaRose@users.noreply.github.com>
2025-06-01 17:26:25 +0100

    Set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Cart from './pages/Cart';
+import Landing from './pages/Landing';
+import Product from './pages/Product';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Nav />
       </header>
+      <Routes>
+<Route exact path="/" element={<Landing />} />
+<Route path="/cart" element={<Cart />} />
+<Route path="/product" element={<Product />} />
+</Routes>
     </div>
   );
 }

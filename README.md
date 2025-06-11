5423ea73bc072706fa3d562d9efd61920787ce98
GlindaRose <45208607+GlindaRose@users.noreply.github.com>
2025-06-11 10:07:08 +0100

    minor updates

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Open the terminal and enter the following command:
 
     npm i https://github.com/lucygossip/l2t11-capstone-project
 
+After installation, navigate to the project folder, then run:
+    
+    npm start
+
 ## Features
 
 - Navbar with links to home, product and cart pages
@@ -17,3 +21,10 @@ Open the terminal and enter the following command:
 - Product cards displaying the product, cost and add to basket button
 - Cart feature that displays total cost and offers different delivery methods
 - A help button and modal offering further information on the delivery methods
+
+## Usage
+
+- Click on the tabs to navigate to different pages
+- Browse through products and add them to your cart
+- View the total cost of your basket
+- For help on delivery methods, click the "Help" button on the cart page
--- src/components/StoreNav.js
+++ src/components/StoreNav.js
@@ -2,30 +2,45 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import LoginForm from "./Forms/LoginForm";
 import Logo from "../images/fruit.jpg";
+import { useState, useEffect } from "react";
 
 // Navbar uses React router to handle links
 export default function StoreNav() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <Navbar bg="dark" expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar variant="dark" bg="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mx-2" />
+        <Navbar.Collapse id="basic-navbar-nav" className={windowWidth < 992 ? "mx-2" : null}>
+          <Nav>
             <Nav.Link as={Link} to="/" className="text-light">
-                <img alt="" src={Logo} className="App-logo" />
-              </Nav.Link>
-            <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/" className="text-light">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/product" className="text-light">
-                Products
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cart" className="text-light">
-                Cart
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-            <LoginForm />
+              {windowWidth < 992 ? (
+                "Home"
+              ) : (
+                <img alt="Logo" src={Logo} className="App-logo" />
+              )}
+            </Nav.Link>
+            <Nav.Link as={Link} to="/product" className="text-light">
+              Products
+            </Nav.Link>
+            <Nav.Link as={Link} to="/cart" className="text-light">
+              Cart
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <LoginForm />
       </Navbar>
     </>
   );
